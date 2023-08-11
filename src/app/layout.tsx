55fbed279751985d75e31413961292c867593d7b
Foziljon Alimov <alimovf2018@gmail.com>
2023-08-12 01:20:15 +0500

    fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>
+      <body suppressHydrationWarning={true} className={montserrat.className}>
         <Navbar />
         <ToastContainer />
         {children}
